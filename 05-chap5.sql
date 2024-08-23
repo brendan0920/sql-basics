4bd27f00c20058acdfacf0c02b332325736e38c7
brendan0920 <brdk1220@gmail.com>
2024-08-22 21:01:22 -0400

    updated 08/22 after homework

--- 05-chap5.sql
+++ 05-chap5.sql
@@ -74,4 +74,181 @@ SELECT VendorName, COUNT(*) AS InvoiceQty, AVG(InvoiceTotal) AS InvoiceAVG
 	ORDER BY InvoiceQty DESC;
 
 --P147 COMPARE having and where clause
-SELECT InvoiceDate, COUNT(*) AS InvoiceQty, SUM(InvoiceTotal) AS InvoiceSum 	FROM Invoices 	GROUP BY InvoiceDate	HAVING InvoiceDate BETWEEN '2023-01-01' 		AND '2023-01-31' 		AND COUNT(*) > 1		AND SUM(InvoiceTotal) > 100 	ORDER BY InvoiceDate DESC;-- same thing using whereSELECT InvoiceDate, COUNT(*) AS InvoiceQty, SUM(InvoiceTotal) AS InvoiceSum 	FROM Invoices 		WHERE InvoiceDate BETWEEN '2023-01-01' AND '2023-01-31' 			GROUP BY InvoiceDate	HAVING  COUNT(*) > 1		AND SUM(InvoiceTotal) > 100 	ORDER BY InvoiceDate DESC;--P148 SQL server extensions - won't see these in MySQL-- ROLLUP operator - add summary rowas at the bottom of result sets--p149SELECT VendorID, COUNT(*) AS InvoiceCount, SUM(InvoiceTotal) AS InvoiceTotal	FROM Invoices 	GROUP BY ROLLUP(VendorID);--p150 CUBE operator--151SELECT VendorID, COUNT(*) AS InvoiceCount, SUM(InvoiceTotal) AS InvoiceTotal	FROM Invoices 	GROUP BY CUBE(VendorID);SELECT VendorState, VendorCity, COUNT(*) AS QtyVendors 	FROM Vendors	WHERE VendorState IN ('IA', 'NJ') 	GROUP BY CUBE (VendorState, VendorCity) 	ORDER BY VendorState DESC, VendorCity DESC;--p156. Exercises--1. two columns from the Invoices table: VendorID and PaymentSum, -- where PaymentSum is the sum of the PaymentTotal column. Group the result set by VendorIDSELECT VendorID, SUM(PaymentTotal) AS PaymentSum	FROM Invoices	GROUP BY VendorID;--2.  SELECT statement that returns two columns: VendorName and PaymentSum, --where PaymentSum is the sum of the PaymentTotal column--Group the result set by VendorName. Return only 10 rows, --corresponding to the 10 vendors who¡¯ve been paid the mostSELECT TOP 10 VendorName, SUM(PaymentTotal) AS PaymentSum	FROM Invoices i	JOIN Vendors v		ON i.VendorID = v.VendorID	GROUP BY VendorName	ORDER BY PaymentSum DESC;-- 3.  Write a SELECT statement that returns three columns: VendorName, InvoiceCount, and InvoiceSum. --InvoiceCount is the count of the number of invoices, and InvoiceSum is the sum of the InvoiceTotal column. --Group the result set by vendor. --Sort the result set so the vendor with the highest number of invoices appears first.SELECT VendorName, COUNT(InvoiceTotal) AS InvoiceCount, SUM(InvoiceTotal) AS InvoiceSum	FROM Invoices i	JOIN Vendors v		ON i.VendorID = v.VendorID	GROUP BY v.VendorID	ORDER BY InvoiceCount DESC;----------------------------------------------------4. SELECT g.AccountDescription, COUNT(*) AS LineItemCount, SUM(il.InvoiceLineItemAmount) AS LineItemSum	FROM GLAccounts g	JOIN InvoiceLineItems il		ON g.AccountNo = il.AccountNo	GROUP BY g.AccountDescription	HAVING COUNT(*) > 1	ORDER BY LineItemCount DESC;--5. Modify the solution to exercise 4 to filter for invoices dated from October 1, 2022 to December 31, 2022.SELECT g.AccountDescription, COUNT(il.AccountNo) AS LineItemCount, SUM(il.InvoiceLineItemAmount) AS LineItemSum	FROM GLAccounts g	JOIN InvoiceLineItems il		ON g.AccountNo = il.AccountNo	JOIN Invoices i		ON i.InvoiceID = il.InvoiceID	WHERE i.InvoiceDate BETWEEN '2022-10-01' AND '2022-12-31'	GROUP BY g.AccountDescription	HAVING COUNT(il.AccountNo) > 1	ORDER BY LineItemCount DESC;--6. What is the total amount invoiced for each AccountNo? -- Use the ROLLUP operator to include a row that gives the grand total.SELECT SUM(InvoiceLineItemAmount) as TotalAmountInvoiced, g.AccountNo	FROM InvoiceLineItems il		JOIN GLAccounts g			ON il.AccountNo = g.AccountNo	GROUP BY ROLLUP (g.AccountNo);--7. Write a SELECT statement four columns: VendorName, Account-Description, LineItemCount, and LineItemSum.--LineItemCount is the row count, and LineItemSum is the sum of the InvoiceLineItemAmount column.--For each vendor and account, return the number and sum of line items, sorted first by vendor, then by account description.SELECT VendorName, Account-Description, COUNT(LineItemCount) AS LineItemCount, SUM(InvoiceLineItemAmount) AS LineItemSum	FROM Vendors v	JOIN  		ON --CHAPTER 6. p160 Subqueries-- Invoices w/ a total greater than the avg InvoiceTotalSELECT InvoiceNumber, InvoiceDate, InvoiceTotal	FROM Invoices	WHERE InvoiceTotal > (		SELECT AVG(InvoiceTotal)			FROM Invoices	)		ORDER BY InvoiceTotal;--p163. Inner jion and a subquery which does the same-- Join invoice and vendors, display the inv#, date, and total for invoices for vendors in CASELECT InvoiceNumber, InvoiceDate, InvoiceTotal 	FROM Invoices i	JOIN Vendors v		ON v.VendorID = i.VendorID	WHERE VendorState = 'CA'	ORDER BY InvoiceDate;-- the same thing with subqueurySELECT InvoiceNumber, InvoiceDate, InvoiceTotal 	FROM Invoices 	WHERE VendorID IN (		SELECT VendorID			FROM Vendors			WHERE VendorState = 'CA'	)	ORDER BY InvoiceDate;
+SELECT InvoiceDate, COUNT(*) AS InvoiceQty, SUM(InvoiceTotal) AS InvoiceSum 
+	FROM Invoices 
+	GROUP BY InvoiceDate
+	HAVING InvoiceDate BETWEEN '2023-01-01' 
+		AND '2023-01-31' 
+		AND COUNT(*) > 1
+		AND SUM(InvoiceTotal) > 100 
+	ORDER BY InvoiceDate DESC;
+
+-- same thing using where
+SELECT InvoiceDate, COUNT(*) AS InvoiceQty, SUM(InvoiceTotal) AS InvoiceSum 
+	FROM Invoices 	
+	WHERE InvoiceDate BETWEEN '2023-01-01' AND '2023-01-31' 		
+	GROUP BY InvoiceDate
+	HAVING  COUNT(*) > 1
+		AND SUM(InvoiceTotal) > 100 
+	ORDER BY InvoiceDate DESC;
+
+--P148 SQL server extensions - won't see these in MySQL
+-- ROLLUP operator - add summary rowas at the bottom of result sets
+--p149
+SELECT VendorID, COUNT(*) AS InvoiceCount, SUM(InvoiceTotal) AS InvoiceTotal
+	FROM Invoices 
+	GROUP BY ROLLUP(VendorID);
+
+--p150 CUBE operator
+--151
+SELECT VendorID, COUNT(*) AS InvoiceCount, SUM(InvoiceTotal) AS InvoiceTotal
+	FROM Invoices 
+	GROUP BY CUBE(VendorID);
+
+SELECT VendorState, VendorCity, COUNT(*) AS QtyVendors 
+	FROM Vendors
+	WHERE VendorState IN ('IA', 'NJ') 
+	GROUP BY CUBE (VendorState, VendorCity) 
+	ORDER BY VendorState DESC, VendorCity DESC;
+
+
+--p156. Exercises
+--1. two columns from the Invoices table: VendorID and PaymentSum, 
+-- where PaymentSum is the sum of the PaymentTotal column. Group the result set by VendorID
+SELECT VendorID, SUM(PaymentTotal) AS PaymentSum
+	FROM Invoices
+	GROUP BY VendorID;
+
+--2.  SELECT statement that returns two columns: VendorName and PaymentSum, 
+--where PaymentSum is the sum of the PaymentTotal column
+--Group the result set by VendorName. Return only 10 rows, 
+--corresponding to the 10 vendors who¡¯ve been paid the most
+SELECT TOP 10 VendorName, SUM(PaymentTotal) AS PaymentSum
+	FROM Invoices i
+	JOIN Vendors v
+		ON i.VendorID = v.VendorID
+	GROUP BY VendorName
+	ORDER BY PaymentSum DESC;
+
+-- 3.  Write a SELECT statement that returns three columns: VendorName, InvoiceCount, and InvoiceSum. 
+--InvoiceCount is the count of the number of invoices, and InvoiceSum is the sum of the InvoiceTotal column. 
+--Group the result set by vendor. 
+--Sort the result set so the vendor with the highest number of invoices appears first.
+SELECT VendorName, COUNT(*) AS InvoiceCount, SUM(InvoiceTotal) AS InvoiceSum
+	FROM Invoices i
+	JOIN Vendors v
+		ON i.VendorID = v.VendorID
+	GROUP BY v.VendorName
+	ORDER BY InvoiceCount DESC;
+
+
+--4. 
+SELECT g.AccountDescription, COUNT(*) AS LineItemCount, SUM(il.InvoiceLineItemAmount) AS LineItemSum
+	FROM GLAccounts g
+	JOIN InvoiceLineItems il
+		ON g.AccountNo = il.AccountNo
+	GROUP BY g.AccountDescription
+	HAVING COUNT(*) > 1
+	ORDER BY LineItemCount DESC;
+
+--5. Modify the solution to exercise 4 to filter for invoices dated from October 1, 2022 to December 31, 2022.
+SELECT g.AccountDescription, COUNT(il.AccountNo) AS LineItemCount, SUM(il.InvoiceLineItemAmount) AS LineItemSum
+	FROM GLAccounts g
+	JOIN InvoiceLineItems il
+		ON g.AccountNo = il.AccountNo
+	JOIN Invoices i
+		ON i.InvoiceID = il.InvoiceID
+	WHERE i.InvoiceDate BETWEEN '2022-10-01' AND '2022-12-31'
+	GROUP BY g.AccountDescription
+	HAVING COUNT(il.AccountNo) > 1
+	ORDER BY LineItemCount DESC;
+
+--6. What is the total amount invoiced for each AccountNo? 
+-- Use the ROLLUP operator to include a row that gives the grand total.
+SELECT SUM(InvoiceLineItemAmount) as TotalAmountInvoiced, g.AccountNo
+	FROM InvoiceLineItems il
+		JOIN GLAccounts g
+			ON il.AccountNo = g.AccountNo
+	GROUP BY ROLLUP (g.AccountNo);
+
+--7. Write a SELECT statement four columns: VendorName, Account-Description, LineItemCount, and LineItemSum.
+--LineItemCount is the row count, and LineItemSum is the sum of the InvoiceLineItemAmount column.
+--For each vendor and account, return the number and sum of line items, sorted first by vendor, then by account description.
+SELECT VendorName, AccountDescription, COUNT(*) AS LineItemCount, SUM(InvoiceLineItemAmount) AS LineItemSum
+	FROM Vendors v
+	JOIN GLAccounts g
+		ON v.DefaultAccountNo = g.AccountNo
+	JOIN InvoiceLineItems il
+		ON g.AccountNo = il.AccountNo
+	join Invoices i
+		ON i.InvoiceID = il.InvoiceID
+	GROUP BY VendorName, AccountDescription
+	ORDER BY VendorName, AccountDescription;
+ 
+
+--vendors - vendorid / AccountNo
+--glaccounts - Accountno / accountdescription
+--invoicelineitems - accountno / invoiceid
+--invoices - invoiceid
+
+--8. which vendors are being paid from more than one account?
+SELECT VendorName, COUNT(DISTINCT il.AccountNo) AS NumberOfAccounts
+	FROM Vendors v
+	JOIN Invoices i
+		ON v.VendorID = i.VendorID
+	JOIN InvoiceLineItems il
+		ON il.InvoiceID =  i.InvoiceID
+	GROUP BY VendorName
+	HAVING COUNT(DISTINCT il.AccountNo) > 1
+	ORDER BY VendorName;
+
+--9.
+SELECT i.VendorID, InvoiceDate, InvoiceTotal,
+		SUM(InvoiceTotal) AS VendorTotal, 
+		COUNT(InvoiceTotal) AS VendorCount, 
+		AVG(InvoiceTotal) AS VendorAvg
+	FROM Invoices i
+	JOIN Vendors v
+		ON i.VendorID = v.VendorID
+	GROUP BY i.VendorID, InvoiceDate, InvoiceTotal;
+
+--answer
+SELECT VendorID, InvoiceDate, InvoiceTotal,
+    SUM(InvoiceTotal) OVER (PARTITION BY VendorID) AS VendorTotal,
+    COUNT(InvoiceTotal) OVER (PARTITION BY VendorID) AS VendorCount,
+    AVG(InvoiceTotal) OVER (PARTITION BY VendorID) AS VendorAvg
+FROM Invoices;
+
+
+
+
+
+--CHAPTER 6. p160 Subqueries
+-- Invoices w/ a total greater than the avg InvoiceTotal
+SELECT InvoiceNumber, InvoiceDate, InvoiceTotal
+	FROM Invoices
+	WHERE InvoiceTotal > (
+		SELECT AVG(InvoiceTotal)
+			FROM Invoices
+	)	
+	ORDER BY InvoiceTotal;
+
+--p163. Inner jion and a subquery which does the same
+-- Join invoice and vendors, display the inv#, date, and total for invoices for vendors in CA
+SELECT InvoiceNumber, InvoiceDate, InvoiceTotal 
+	FROM Invoices i
+	JOIN Vendors v
+		ON v.VendorID = i.VendorID
+	WHERE VendorState = 'CA'
+	ORDER BY InvoiceDate;
+
+-- the same thing with subqueury
+SELECT InvoiceNumber, InvoiceDate, InvoiceTotal 
+	FROM Invoices 
+	WHERE VendorID IN (
+		SELECT VendorID
+			FROM Vendors
+			WHERE VendorState = 'CA'
+	)
+	ORDER BY InvoiceDate;
+
